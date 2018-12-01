e51706b150af5f4f8dc56371652998cc0652db84
Ashwani Dubey <ashwanikumardubey1@gmail.com>
2018-12-01 17:06:46 +0530

    Added autofill options

--- autocomplete.js
+++ autocomplete.js
@@ -1,29 +1,47 @@
 $(document).ready(function(){
 var $div = $('<div />').appendTo('body');
 $div.attr('id', 'autocompletesuggestions');
-$('head').append('<style>.autoselected{background-color:#3390FF;color:#fff} #autocompletesuggestions{position:absolute;z-index:10} #autocompletesuggestions div{padding:5px;cursor:pointer;border-bottom:1px solid rgba(240,240,240,1);}</style>')
+$('head').append('<style>.autoselected{background-color:#3390FF;color:#fff} #autocompletesuggestions{background-color:white;position:absolute;z-index:10} #autocompletesuggestions div{padding:5px;cursor:pointer;border-bottom:1px solid rgba(240,240,240,1);}</style>')
 });
 
 jQuery.fn.autocomplete = function(options){
 var target = $(this);    
-console.log($(this).outerHeight());
-
-$(target).on('focusin',function(){
-  var position = $(this).position();
-  var width = $(this).outerWidth();
-  var height = $(this).outerHeight();
-  console.log(position);console.log(height);
-  
-  
+var autofill = options.autofill!=undefined?options.autofill:true;    
+    
+target.attr('autocomplete','off');
+target.on('focusin',function(){
+  var position = target.position();
+  var width = target.outerWidth();
+  var height = target.outerHeight();
+    
   $("#autocompletesuggestions").css({"width":width+"px","height":"auto","top":position.top+height+"px","left":position.left+"px"});
- }).on('keyup',function(){
-  this.completeArray = jQuery.grep(options.array, function( item ) {
+ }).on('keyup',function(e){
+    console.log(e);
+    this.completeArray = jQuery.grep(options.array, function( item ) {
       return item.toLowerCase().includes(target.val().toLowerCase());
     });
     var html = "";
+    var foundSimilar = false;
+    var str = "";
+    var start;
+    var end;
     $.each(this.completeArray,function(index,item){
       html += '<div>'+item+'</div>';
+      if(!foundSimilar){
+            if(item.toLowerCase().indexOf(target.val().toLowerCase()) == 0 && target.val()!=""){
+               start = target.val().length;
+               end = item.length;
+                str = item.substring(start,end);
+                foundSimilar = true;
+
+            }
+      }
     });
+    
+    if(autofill && foundSimilar && checkautoFill(e)){
+        target.val(target.val().concat(str));
+        $(target).selectRange(start,end);
+    }
     $("#autocompletesuggestions").html(html).css({"border":"1px solid rgba(240,240,240,1)"}); 
     
     $("#autocompletesuggestions div").hover(function(){
@@ -41,4 +59,30 @@ $(target).on('focusin',function(){
     $("#autocompletesuggestions").hide();
   });
 
+
+  $.fn.selectRange = function(start, end) {
+    return this.each(function() {
+        if (this.setSelectionRange) {
+            this.focus();
+            this.setSelectionRange(start, end);
+        } else if (this.createTextRange) {
+            var range = this.createTextRange();
+            range.collapse(true);
+            range.moveEnd('character', end);
+            range.moveStart('character', start);
+            range.select();
+        }
+    });
+}; 
+
+function checkautoFill(e){
+    if(e.keyCode <= 46 || (e.keyCode >= 112 && e.keyCode <= 123) || e.ctrlKey)
+        return false;
+    else
+        return true;
+}
+
+
+
 };
+
